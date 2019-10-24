1636392799600ac103063d3fe1735aafff4dfdda
Rui Correia <rmgc31@gmail.com>
2019-10-24 20:29:31 +0100

    feed method added

--- src/virtual-pet.js
+++ src/virtual-pet.js
@@ -1,11 +1,34 @@
+const MAXIMUM_FITNESS = 10;
+const MINIMUM_HUNGER = 0;
+
 function Pet (name) {
     this.name = name;
     this.age = 0;
+    this.hunger = 0;
+    this.fitness = MAXIMUM_FITNESS;
 };
 
 Pet.prototype = {
     growUp() {
         this.age += 1;
+        this.hunger += 5;
+        this.fitness -= 3;
+    },
+
+    walk() {
+        if (this.fitness > 6) {
+            this.fitness = MAXIMUM_FITNESS;
+        } else {
+            this.fitness += 4;
+        }
+    },
+
+    feed() {
+        if (this.hunger <= 3) {
+            this.hunger = MINIMUM_HUNGER;
+        } else {
+            this.hunger -=3;
+        }
     }
 }
 
--- test/virtual-pet.test.js
+++ test/virtual-pet.test.js
@@ -20,5 +20,49 @@ describe('create a new object with a set of properties', () => {
         const pet = new Pet('Fido');
         pet.growUp();
         expect(pet.age).toEqual(1);
-    })
-})
+    });
+
+    test('return the initial hunger of the pet', () => {
+        const pet = new Pet('Fido');
+        expect(pet.hunger).toEqual(0);
+    });
+
+    test('returns the hunger incremented by 5', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.hunger).toEqual(5);
+    });
+
+    test('return the initial fitness of the pet', () => {
+        const pet = new Pet('Fido');
+        expect(pet.fitness).toEqual(10);
+    });
+
+    test('returns the fitness decremented by 3', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.fitness).toEqual(7);
+    });
+
+    test('return the fitness equal to 10, when it is above 6', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        pet.walk();
+        expect(pet.fitness).toEqual(10);
+    });
+
+    test('returns the new fitness value, when below 6', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        pet.growUp();
+        pet.walk();
+        expect(pet.fitness).toEqual(8);
+    });
+
+    test('returns hunger level decreased by feeding the pet', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        pet.feed();
+        expect(pet.hunger).toEqual(2);
+    });
+});
