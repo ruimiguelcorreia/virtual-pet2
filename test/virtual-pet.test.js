cac4b6ddcfc781338d93661e3b42f1a6c7c56770
Rui Correia <rmgc31@gmail.com>
2019-10-24 20:55:36 +0100

    status method added

--- test/virtual-pet.test.js
+++ test/virtual-pet.test.js
@@ -1,7 +1,6 @@
 const Pet = require("../src/virtual-pet");
 
 describe('create a new object with a set of properties', () => {
-    
     test('create a new object', () => {
         expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
@@ -65,4 +64,35 @@ describe('create a new object with a set of properties', () => {
         pet.feed();
         expect(pet.hunger).toEqual(2);
     });
+
+    test('returns an alert when the fitness level is low', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        pet.growUp();
+        pet.growUp();
+        pet.feed();
+        pet.feed();
+        pet.feed();
+        pet.feed();
+        expect(pet.checkUp()).toBe('I need a walk!');
+    });
+
+    test('returns an alert when the pet is hungry', () => {
+        const pet = new Pet('Fido');
+        pet.growUp()
+        expect(pet.checkUp()).toBe('I am hungry!');
+    });
+
+    test('returns an alert when the fitness is low and pet is hungry', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        pet.growUp();
+        pet.growUp();
+        expect(pet.checkUp()).toBe('I am hungry and I need a walk!');
+    });
+
+    test('returns a state of happiness when the pet levels are ok', () => {
+        const pet = new Pet('Fido');
+        expect(pet.checkUp()).toBe('I feel great!');
+    })
 });
