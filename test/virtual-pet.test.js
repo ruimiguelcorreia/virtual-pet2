dccb56869fee7dfebc231f720c8c3f936adbe46e
Rui Correia <rmgc31@gmail.com>
2019-10-24 18:47:11 +0100

    first property added

--- test/virtual-pet.test.js
+++ test/virtual-pet.test.js
@@ -2,7 +2,12 @@ const Pet = require("../src/virtual-pet");
 
 describe('create a new object with a set of properties', () => {
     
-    test('create the object', () => {
+    test('create a new object', () => {
         expect(new Pet('Fido')).toBeInstanceOf(Object);
+    });
+
+    test('return the object with a name property', () => {
+        const pet = new Pet('Fido');
+        expect(pet.name).toStrictEqual('Fido');
     })
 })
